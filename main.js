45041e429b5eaae8686e77293e4cc0154f364180
KabirTheOne <96368363+KabirTheOne@users.noreply.github.com>
2022-05-04 13:31:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var name = "Ranbir";
+var lname = "Kapoor";
+console.log("My first console project")
+console.log(name.concat(lname))
+console.log(name.toLowerCase())
+console.log(lname.toUpperCase())
+var x = 10
+var y = 5
+var z = x + y
+console.log(Math.max(x, y))
+console.log(Math.min(x, y))
+console.log(z)
+console.log(Math.sqrt(144))
+console.log(Math.round(387.23))
